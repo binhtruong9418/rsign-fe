7dd00133af38f8d5902e1b17fa55e1deb3e52a55
binhtruong9418 <binhtruong9418@gmail.com>
2025-10-01 21:53:45 +0700

    fix home page UI

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -17,7 +17,7 @@ const HomePage: React.FC = () => {
                 <div className="container relative mx-auto">
                     <div className="items-center flex flex-wrap">
                         <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-                            <div className="pr-12">
+                            <div>
                                 <h1 className="text-white font-semibold text-5xl">
                                     Sign Documents, Seamlessly.
                                 </h1>
